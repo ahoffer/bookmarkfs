20768d6f5e85700231b6550e92d583168b33ed52
Aaron Hoffer <aaron.hoffer@octo.us>
2025-04-16 11:22:38 -0700

    Added pom file

---
+++ src/main/java/com/example/bookmarks/BookmarkTreeApplication.java
@@ -0,0 +1,11 @@
+package com.example.bookmarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkTreeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookmarkTreeApplication.class, args);
+    }
+}
